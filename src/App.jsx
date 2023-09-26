d5a8efbab37872ce6e24b884a4038e4b2867342a
andrea <andregabriela41@gmail.com>
2023-09-25 23:55:44 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,25 @@
+import phrases from "./db/phrases.json";
+import "./App.css";
+import { useState } from "react";
+import { random } from "./utils/random";
+import Phrases from "./components/Phrases";
 
-import phrases from './db/phrases.json'
-import './App.css'
-import {useState} from 'react'
-import {random} from './utils/random'
-import Phrases from './components/Phrases'
-
-
-
-const imgs = ['img1', 'img2', 'img3', 'img4']
+const imgs = ["img1", "img2", "img3", "img4"];
 
 function App() {
-const[backgroundImg, setBackgroundImg] = useState(random(imgs))
+  const [backgroundImg, setBackgroundImg] = useState(random(imgs));
 
-
-const [note, setNote] = useState(random(phrases))
-const handlePhrases = () => {
-setNote(random(phrases))
-setBackgroundImg(random(imgs))
-}
- 
+  const [note, setNote] = useState(random(phrases));
+  const handlePhrases = () => {
+    setNote(random(phrases));
+    setBackgroundImg(random(imgs));
+  };
 
   return (
     <main className={`viewbg ${backgroundImg}`}>
-    
-     <Phrases handlePhrases={handlePhrases} note={note} />
-    
+      <Phrases handlePhrases={handlePhrases} note={note} />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Phrases.jsx
+++ src/components/Phrases.jsx
@@ -1,23 +1,18 @@
-import React from 'react'
-import './styles/Phrases.css'
+import "./styles/Phrases.css";
 
-
-
-const Phrases = ({handlePhrases, note}) => {
+const Phrases = ({ handlePhrases, note }) => {
   return (
-    <section className='container'>
-     <h1>Fortune Cookie</h1>
-    <section className='container1'>
-    <article>
-        <p>
-            {note.phrase}
-        </p>
-    </article>
-     <button onClick={handlePhrases}>Next Cookie</button>
+    <section className="container">
+      <h1>Fortune Cookie</h1>
+      <section className="container1">
+        <article>
+          <p>{note.phrase}</p>
+        </article>
+        <button onClick={handlePhrases}>Next Cookie</button>
+      </section>
+      <footer>Fuente: {note.author}</footer>
     </section>
-    <footer>Fuente: {note.author}</footer>
-    </section>
-  )
-}
+  );
+};
 
-export default Phrases
+export default Phrases;
